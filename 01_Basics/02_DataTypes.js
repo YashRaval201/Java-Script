7a1f60f6476d5567cb21ac673efb0485b12f33b5
Yash Raval <70638712+YashRaval201@users.noreply.github.com>
2024-06-16 19:55:20 +0000

    DataType

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") We're using node js not a browser so that it open prompt
+
+console.log(3 + 3); 
+
+console.log("yash");
+
+let name = "yashraval"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// string => " "
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "yash");
+console.log(typeof null);
