085eaa58552ce592c0796846046eeb4efc5589f8
Yash Raval <70638712+YashRaval201@users.noreply.github.com>
2024-06-16 19:36:50 +0000

    Variable File created to know variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "yashraval@gmail.com"
+var accountPasswrod = "12345"
+accountCity = "Patan"
+let accountState
+// accountId = 2 //Not allowed to change constant variables
+
+accountEmail = "hc@hc.com"
+accountPasswrod = 32156
+accountCity =  "Ahmedabad"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPasswrod, accountCity, accountState])
+/*Do not use var ever*/
