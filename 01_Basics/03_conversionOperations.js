2b2b910b62107a6c9e8a0000dcba2d30d47587ad
Yash Raval <70638712+YashRaval201@users.noreply.github.com>
2024-06-17 13:26:45 +0000

    Conversion of one datatype to another datatype.

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "20"
+let score1 = "20yash"
+let score2 = null
+let score3 = true
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log("score 20 in Number : ",valueInNumber);
+// console.log("Type of score : ",typeof (valueInNumber));
+// console.log("score1 20yash in Number : ",score1);
+// console.log("Type of score : ",typeof (score1));
+// console.log("score null in Number : ",score2);
+// console.log("Type of score : ",typeof (score2));
+// console.log("score true in Number : ",score3);
+// console.log("Type of score : ",typeof (score3));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIN = 1
+let booleanIsLoggedeIn = Boolean(isLoggedIN)
+console.log(booleanIsLoggedeIn)
+
+// 1 => true; 0 => false
+// "" => false; "yash" => true
+
+let someNumber = 33
+let  stringNumber = String(someNumber) 
+console.log(stringNumber);
